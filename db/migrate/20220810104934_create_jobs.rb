e3c0e5def2170d1254a06a1286923e29335ff153
pilar sanchez <mpilar-93@hotmail.com>
2022-08-10 11:50:01 +0100

    created db

---
+++ db/migrate/20220810104934_create_jobs.rb
@@ -0,0 +1,13 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :position
+      t.string :company
+      t.string :link
+      t.string :status
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
